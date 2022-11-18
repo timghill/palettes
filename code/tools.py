2af320c4969f9e5ce2d97bfb5ba529c4e344f2eb
Tim Hill <tha111@sfu.ca>
2022-11-17 20:45:27 -0800

    Fix size bug

--- code/tools.py
+++ code/tools.py
@@ -49,11 +49,12 @@ def join_cmaps(cm1, cm2, N1=128, N2=128, name='Joined Cmap', average=False):
     
     cm_smooth = np.zeros(cm_data.shape)
     if average:
-        for j in range(cm_smooth.shape[4]):
+        for j in range(cm_smooth.shape[1]):
             col = cm_data[:, j]
-            cm_smooth[:len(col), j] = np.convolve(col, np.ones(average))/average
+            smooth_col = np.convolve(col, np.ones(average), 'valid')/average
+            cm_smooth[:len(smooth_col), j] = smooth_col
     
-        cm_data = cm_smooth[:len(col)]
+        cm_data = cm_smooth[:len(smooth_col)]
 
     cm = LinearSegmentedColormap.from_list(name, cm_data)
 
